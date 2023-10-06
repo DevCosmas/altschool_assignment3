6c2649795bf65736163d2fe8fca6e3d63f901f46
UgwuCosmas chibuike <buikem0112@gmail.com>
2023-10-06 21:11:23 +0100

    connection ~ ~ ~ ~ ~ ~ ~ ~ ~ .git/COMMIT_EDITMSG[+] [unix] (21:11 06/10/2023) 25,1 All  data base connection

--- mongoose/routes/user_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { signUp, logIn} = require('./../controllers/user_controller')
-
-const router = express.Router();
-
-
-router.post('/sign_up', signUp)
-router.post('/logIn', logIn)
-module.exports = { router }
--- mongoose/utils/global_middleware.js
+++
@@ -1,8 +0,0 @@
-// CHECK FOR REQUEST BODY
-const requestBody = (req, res, next) => {
-    if (!req.body) res.status(400).json({ result: "FAIL", message: "request body is empty" })
-
-    next()
-}
-
-module.exports = { requestBody }
